3180bbf5b1a18a6813398bdef5f482ebfc09f453
BichPhuong <21130488@st.hcmuaf.edu.vn>
2025-02-17 09:23:12 +0700

    add review for product

--- src/main/java/com/ecommerce/book_store/http/admin/controller/auth/BookController.java
+++ src/main/java/com/ecommerce/book_store/http/admin/controller/auth/BookController.java
@@ -1,25 +1,35 @@
 package com.ecommerce.book_store.http.admin.controller.auth;
 
-import com.ecommerce.book_store.persistent.entity.Book;
+import com.ecommerce.book_store.persistent.entity.Review;
 import com.ecommerce.book_store.service.abstraction.BookService;
+import com.ecommerce.book_store.service.abstraction.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/books")
 public class BookController {
-    @Autowired
-    private BookService bookService;
+    private final BookService bookService;
+    private final ReviewService reviewService;
+
+    public BookController(BookService bookService, ReviewService reviewService) {
+        this.bookService = bookService;
+        this.reviewService = reviewService;
+    }
+
     @GetMapping("/{id}")
     public String getProductDetail(@PathVariable Long id, Model model) {
         return bookService.getBookById(id)
                 .map(book -> {
                     model.addAttribute("book", book);
+                    List<Review> reviews = reviewService.getReviewsByBookId(id);
+                    model.addAttribute("reviews", reviews);
                     return "pages/auth/detailBook";
                 })
                 .orElse("error/404");
