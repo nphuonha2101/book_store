4fe54e142500d07ccf49b77fc3d3aa6cd13bbc2a
BichPhuong <21130488@st.hcmuaf.edu.vn>
2025-02-27 22:52:39 +0700

    edit new slider

--- src/main/java/com/ecommerce/book_store/http/dto/request/implement/SliderRequestDto.java
+++ src/main/java/com/ecommerce/book_store/http/dto/request/implement/SliderRequestDto.java
@@ -3,12 +3,13 @@ package com.ecommerce.book_store.http.dto.request.implement;
 import com.ecommerce.book_store.http.dto.request.AbstractRequestDto;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Setter
 @Getter
 public class SliderRequestDto extends AbstractRequestDto {
     private String title;
     private String description;
-    private String image;
+    private MultipartFile image;
     private String url;
 }
