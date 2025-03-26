b1cb63f01e6be8c7be105c6a6a5a09d7eaf41db8
Phuong Nha Nguyen <nphuonha2101@gmail.com>
2025-03-27 01:53:46 +0700

    feat: enhance OIDC user service to include avatar and password encoding for new users

--- src/main/java/com/ecommerce/book_store/service/oauth/CustomOidcUserService.java
+++ src/main/java/com/ecommerce/book_store/service/oauth/CustomOidcUserService.java
@@ -3,30 +3,39 @@ package com.ecommerce.book_store.service.oauth;
 import com.ecommerce.book_store.persistent.entity.User;
 import com.ecommerce.book_store.persistent.repository.abstraction.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class CustomOidcUserService extends OidcUserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public CustomOidcUserService(UserRepository userRepository) {
+    public CustomOidcUserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
         OidcUser oidcUser = super.loadUser(userRequest);
         String email = oidcUser.getAttribute("email");
+        String avatar = oidcUser.getAttribute("picture");
+        System.out.println(oidcUser.getAttributes());
 
         User user = userRepository.findByEmail(email)
                 .orElseGet(() -> {
                     User newUser = new User();
                     newUser.setEmail(email);
+                    newUser.setAvatar(avatar);
+                    newUser.setPassword(passwordEncoder.encode(UUID.randomUUID().toString()));
                     newUser.setName(oidcUser.getAttribute("name"));
                     return userRepository.save(newUser);
                 });
