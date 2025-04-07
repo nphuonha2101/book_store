d0277d1ee247b561fe7fc94a582c52a1e74df582
BichPhuong <21130488@st.hcmuaf.edu.vn>
2025-04-07 15:51:10 +0700

    #33 feat: write request response Address

---
+++ src/main/java/com/ecommerce/book_store/http/dto/response/implement/AddressResponseDto.java
@@ -0,0 +1,24 @@
+package com.ecommerce.book_store.http.dto.response.implement;
+
+import com.ecommerce.book_store.http.dto.response.AbstractResponseDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+@Setter
+public class AddressResponseDto extends AbstractResponseDto {
+    private Long id;
+    private Long userId;
+    private String fullName;
+    private String phone;
+    private String province;
+    private String district;
+    private String ward;
+    private String addInfo;
+    private boolean isDefault;
+
+
+
+}
