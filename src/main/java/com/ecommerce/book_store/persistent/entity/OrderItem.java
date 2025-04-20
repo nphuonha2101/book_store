3affc7069322188fde5e16a165eecd76b8d16b96
Phuong Nha Nguyen <nphuonha2101@gmail.com>
2025-04-20 14:13:41 +0700

    resolve conflict

--- src/main/java/com/ecommerce/book_store/persistent/entity/OrderItem.java
+++ src/main/java/com/ecommerce/book_store/persistent/entity/OrderItem.java
@@ -25,5 +25,15 @@ public class OrderItem extends AbstractEntity implements Serializable {
     @Column(name = "price")
     private double price;
 
+    public OrderItem() {
+    }
+
+    public OrderItem(Order order, Book book, int quantity, double price) {
+        this.order = order;
+        this.book = book;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
 
 }
